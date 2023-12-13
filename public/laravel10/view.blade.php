97056e6057a11c41fbb98ffce17d0bb4199addeb
Hafiz Haziq <hafizhaziq.dev@gmail.com>
2023-12-14 00:49:04 +0800

    Update view.blade.php

--- public/laravel10/view.blade.php
+++ public/laravel10/view.blade.php
@@ -85,19 +85,28 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="card">
-                                <header class="card-header my-1 py-1">
-                                    <div class="row">
-                                        <div class="col-sm-10 mt-2">
-                                            <h3 class="card-title">@@@crudtitle@@@</h3>
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <button type="button" class="btn btn-primary float-sm-right" data-toggle="modal" data-target="#addModal" id="addBtn">
-                                                <i class="fas fa-plus"></i>
-                                                <span>Add</span>
-                                            </button>
-                                        </div>
+                                <header class="card-header text-right">
+                                    <button type="button" class="btn btn-sm btn-primary" data-toggle="modal"
+                                        data-target="#addModal">
+                                        <span class="mr-1">Tambah Rekod Baru</span>
+                                        <i class="fas fa-plus"></i>
+                                    </button>
+                                </header>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="card">
+                                <header class="card-header">
+                                    <h3 class="card-title">@@@crudtitle@@@</h3>
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"
+                                            title="Collapse">
+                                            <i class="fas fa-minus"></i>
+                                        </button>
                                     </div>
                                 </header>
+                                
                                 <div class="card-body">
                                     <div class="table-responsive">
                                         <table id="table" class="table table-sm table-striped table-hover table-bordered nowrap" style="width: 100%">
